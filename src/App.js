1daca2bb0a80c186335fa55d55fead578fe6f89d
KryNak <krystian.nakielski200397@gmail.com>
2021-12-11 19:16:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="background">
+      <img className="logo" height="100%" src="logo.svg" alt="logo"></img>
+      <div className="container">
+        <img alt="phone" src="mock.png"></img>
+        <div className="rect"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
