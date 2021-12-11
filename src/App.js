66f32d669aeb5cd20880820a342ef4ee2029b702
KryNak <krystian.nakielski200397@gmail.com>
2021-12-11 19:47:02 +0100

    Added images

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="background">
+
       <img className="logo" height="100%" src="logo.svg" alt="logo"></img>
+      
       <div className="container">
         <img alt="phone" src="mock.png"></img>
-        <div className="rect"></div>
+        <div className="rect">
+          <div className="join">Join today - it's free!</div>
+          <img src="android.png" alt="android"></img>
+          <img src="ios.png" alt="ios"></img>
+          <br></br>
+          <img src="facebook.svg" alt="facebook"></img>
+          <img src="meta.svg" alt="meta"></img>
+          <img src="instagram.svg" alt="instagram"></img>
+          <img src="linkedin.svg" alt="linkedin"></img>
+          <img src="twitter.svg" alt="twitter"></img>
+        </div>
       </div>
+
     </div>
   );
 }
